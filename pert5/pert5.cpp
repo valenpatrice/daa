f87693ca6d08e23fdbb3f4c04747533bbfd98f18
root <root@LAPTOP-HLCH1L53>
2023-04-18 11:23:10 +0700

    first

---
+++ pert5/pert5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int mai(){
+    int a = 20;
+    int b = 10;
+
+    cout<<"Contoh if else Statement"<<endl;
+
+    if(a >b){
+        cout<<"a lebih besar dari b"<<endl;
+    }
+    else {
+        cout<<"b lebih besar dari a"<<endl;
+    }
+    return 0;
+}
